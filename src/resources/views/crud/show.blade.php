bf01fcd27bdabc0ccd516e59db2d5fdf7c064d1c
starmoozie@gmail.com <starmoozie@gmail.com>
2021-04-10 02:49:49 +0700

    update show list responsive

--- src/resources/views/crud/show.blade.php
+++ src/resources/views/crud/show.blade.php
@@ -47,7 +47,7 @@
 				</div>
 			</div>
 	    @endif
-	    <div class="card no-padding no-border">
+	    <div class="card no-padding no-border table-responsive">
 			<table class="table table-striped mb-0">
 		        <tbody>
 		        @foreach ($crud->columns() as $column)
