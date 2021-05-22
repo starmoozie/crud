1178fe1edb570202e107536c33ffe29a0f2e95a0
Starmoozie <starmoozie@gmail.com>
2021-05-23 01:32:56 +0700

    Update base.php

--- src/config/starmoozie/base.php
+++ src/config/starmoozie/base.php
@@ -71,7 +71,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => env('APP.NAME'),
+    'project_logo'   => env('APP_NAME'),
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
